d6e2a1c743ba1ee812f93725bd327bd9d5a28c2f
William Boswall <wboswall@live.com>
2022-06-16 19:53:57 -0300

    Committing 0.0.2.0 update also created content.php made minor changes to main.css avonlea.js and header.php made major changes to index.php and content.php

---
+++ phpScripts/content.php
@@ -0,0 +1,13 @@
+<? echo ('<article class="mainPg">
+					<h1>Welcome!</h1>
+					<p> I am currently building the website from scratch and there will be more content and pages to come in the near future! </p>
+                    <section class="bio">
+					<p> Hello, I am William Boswall and from PEI, Canada.  I was born and raised on PEI on my family&#39;s mixed farm.  The farm comprised of potatoes, grain, beef, pigs, and dairy.  Also lots of cats to deter mice, rats and other varments.  
+                    But over the years due to markets declining, we decided to call it quits around 2013 or so. </p>
+                    <p> I have a BA in Arts from UPEI (Class of 2012).  I majored in Sociology and a minor in English. However I decided that I wanted to take courses in IT back in 2013.  However I decided after doing some research that web programming is the best way to go. 
+                    I then decided back in 2019 to take a course in HTML/CSS for web design at Academy of Learning and recieved the certificate in March 2020.  I plan to take more courses to stay current in the ever evolving web development field.  I am currently learning C Language, C PLus Plus, Javascript, PhP and Java.  
+                    </section>
+				</article>
+				');
+?>
+<? echo("<p>todo</p>"); ?>
