68b225cc80e3dbb48173aa910e9d6077d21fd48d
William Boswall <wboswall@live.com>
2022-07-12 08:41:54 -0300

    Update index.php
    added the barebones html template

---
+++ demodotwboswalldotca/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-CA">
+    <head>
+        <meta charset="UTF-8">
+        <title>Page Title</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="">
+        <style>
+        </style>
+    </head>
+    <body>
+
+    <img src="img_la.jpg" alt="Placeholder" style="width:100%">
+
+    <div class="">
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+    <p>This is another paragraph.</p>
+    </div>
+    <script src=""></script>
+    <script src="" defer></script>
+    <script src="" async></script>
+    </body>
+</html>
